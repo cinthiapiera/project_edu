53eb65b6c5a40405af4b8f61cf90caea1030432f
Cinthiapiera <cinthia.maldonado@upch.pe>
2024-08-04 18:38:08 -0500

    Complete header structure and styling

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-'use strict';
+'use strict'; 
 
 const overlay = document.querySelector("[data-overlay]");
 const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbar= document.querySelector("[data-navbar]")
+const navbar = document.querySelector("[data-navbar]");
 const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-const navLinks = document.querySelectorAll("data-nav-link");
-
-// console.log(overlay);
-// console.log(navOpenBtn);
-// console.log(navbar);
-// console.log(navCloseBtn);
-// console.log(navLinks);
+const navLinks = document.querySelectorAll("[data-nav-link]"); 
 
 const navElemArr = [navOpenBtn, navCloseBtn, overlay];
 
 const navToggleEvent = function (elem) {
-    for(let i=0; i<elem.length; i++){
-        elem[i].addEventListener("click", function(){
-            navbar.classList.toggle("active");
+    for (let i = 0; i < elem.length; i++) {
+        elem[i].addEventListener("click", function () {
+            navbar.classList.toggle("active"); 
             overlay.classList.toggle("active");
         });
     }
-}
+};
 
 navToggleEvent(navElemArr);
 navToggleEvent(navLinks);
 
-const header = document.querySelector("[data-header]");
+const header = document.querySelector("[data-header]"); 
 window.addEventListener("scroll", function () {
-   if(window.scrollY >= 200){
-    header.classList.add("active");
-   }else{
-    header.classList.add("active");
-   } 
+    if (window.scrollY >= 200) { 
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active"); 
+    }
 });
